cab4b22b96546e6f245ab412e25ce42c1b7e7b4f
Kth <vcro12123@naver.com>
2022-06-05 20:11:56 +0900

    redux/toolkit 복습

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,8 +1,16 @@
 import classes from './CartItem.module.css';
-
+import { useDispatch } from 'react-redux';
+import { cartActions } from '../../store/cart-slice';
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
-
+  const dispatch = useDispatch();
+  const { title, quantity, total, price, id } = props.item;
+  console.log();
+  const addHandler = () => {
+    dispatch(cartActions.addItemToCart({ id, title, price }));
+  };
+  const removeHandler = () => {
+    dispatch(cartActions.removeItemFromCart(id));
+  };
   return (
     <li className={classes.item}>
       <header>
@@ -17,8 +25,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeHandler}>-</button>
+          <button onClick={addHandler}>+</button>
         </div>
       </div>
     </li>
